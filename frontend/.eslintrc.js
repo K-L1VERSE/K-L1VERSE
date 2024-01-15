a744e874744ff66a122d33d30f0206b5ba807e6d
lion0077v <lion0077v@gmail.com>
2024-01-15 23:46:17 +0900

    ⚠️Initiate React Project
    프로젝트 명 : frontend
    React ver. : 18.2.0
    React-dom ver. : 18.2.0
    JavaScript
    eslint + prettier
    
    eslint + prettier 설정으로 인해 반드시 커밋 전에 터미널로 아래 작업을 수행해 주세요!
    
    ```bash
    // 오류가 발생한 파일을 알려준다.
    $ npm run format
    // 오류가 발생한 파일을 규칙에 맞게 고쳐준다.
    $ npm run format:fix
    ```
    
    [Resolves] S10P12A409-119
    
    ⚠️Initiate React Project

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "no-nested-ternary": 0,
+  },
+};
---
+++ frontend/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  doubleQuote: true, // 문자열을 사용 할 때에는 " 를 사용
+  semi: true, // 코드 종료시 무조건 ;(세미콜론)
+  useTabs: false, // tab 대신에 스페이스를 사용
+  tabWidth: 2, // 들여쓰기 크기
+  trailingComma: "all", // 객체나 배열의 마지막 값에도 , 를 붙임
+  printWidth: 80, // 한 줄의 최대 칸수
+};
