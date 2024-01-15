b681d89f82d590b6239ce5963dcb27c739a2900b
lion0077v <lion0077v@gmail.com>
2024-01-16 00:44:13 +0900

    [Style] react 폴더 기본구조
    저장한 모든 파일은 예시입니다.
    
    각 개발하는 도메인에 맞게 폴더를 새로 만들고 그 안에서 파일을 작업해주시면 됩니다.
    
    [Resolves] S10P12A409-122

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -36,5 +36,6 @@ module.exports = {
     "react/jsx-one-expression-per-line": 0,
     "no-nested-ternary": 0,
     "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "import/prefer-default-export": "off",
   },
 };
