af60edf9c8a19570e71fa7e8cbe8ac8b80225490
홍윤기 <lion0077v@gmail.com>
2024-01-16 17:36:19 +0900

    [Chore] react 설정 변경
    "axios", "react-router-dom" 추가
    
    ".env" 추가
    
    eslint 설정 추가

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -38,4 +38,12 @@ module.exports = {
     "prettier/prettier": ["error", { endOfLine: "auto" }],
     "import/prefer-default-export": "off",
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        paths: ["src"],
+      },
+    },
+  },
 };
