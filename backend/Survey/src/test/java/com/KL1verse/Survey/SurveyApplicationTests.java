ec7bb13829bb15490a375a1323e81711f859475b
JeonSeongJae <jeonseongjae@naver.com>
2024-01-16 12:22:34 +0900

    Survey 서버

---
+++ backend/Survey/src/test/java/com/KL1verse/Survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KL1verse.Survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
