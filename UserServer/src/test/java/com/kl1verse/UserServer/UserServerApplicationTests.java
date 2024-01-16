0ff5581baadabb39e32f9dda5c73f0b74b95bd31
KimDongHun <huni19541@gmail.com>
2024-01-16 09:09:26 +0900

    UserServer

---
+++ UserServer/src/test/java/com/kl1verse/UserServer/UserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl1verse.UserServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
