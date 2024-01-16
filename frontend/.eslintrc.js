8e58b460591bd981f6d6430edbde0c3710cd29fc
홍윤기 <lion0077v@gmail.com>
2024-01-16 17:36:19 +0900

    [Fix] 4 -> develop 충돌 해결
    "axios", "react-router-dom" 추가
    
    ".env" 추가
    
    eslint 설정 추가

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -53,4 +53,12 @@ module.exports = {
       },
     },
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        paths: ["src"],
+      },
+    },
+  },
 };
