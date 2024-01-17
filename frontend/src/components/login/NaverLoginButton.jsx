ae1dae9992c1b190e70298c920d4feb833d0b3dc
홍윤기 <lion0077v@gmail.com>
2024-01-17 15:04:15 +0900

    [Feat] 네이버 로그인 API 연결
    -
    
     네이버 로그인 버튼 만들기
    
    [Resolves] S10P12A409-198
    [Ref]
    [Realted to]

---
+++ frontend/src/components/login/NaverLoginButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NaverLogin() {
+  // const NAVER_RESPONSE_TYPE = "code"
+  const NAVER_REDIRECT_URI = "http://localhost:3000/NaverAuth/";
+  const NAVER_STATE = "1234asdf";
+  // const NAVER_SCOPE = "profile"
+  const NAVER_CLIENT_ID = "eXJ_cTAVvlCn3lXE5sY6";
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${NAVER_STATE}`;
+
+  return (
+    <a href={NAVER_AUTH_URL}>
+      <button type="button">네이버 로그인</button>
+    </a>
+  );
+}
+
+export default NaverLogin;
