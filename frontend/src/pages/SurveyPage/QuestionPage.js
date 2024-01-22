39ca1cd9e573157f0a79af27fbad27aac8dfac51
홍윤기 <lion0077v@gmail.com>
2024-01-22 12:43:13 +0900

    [Fix] 충돌 완전 해결
    Former-commit-id: a9f2debd045af66290dc9d5aff2536663f45496f

--- frontend/src/pages/SurveyPage/QuestionPage.js
+++ frontend/src/pages/SurveyPage/QuestionPage.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 // import QuizCard from "../../components/Survey/Quizcard";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
-<<<<<<< HEAD
 import { submitResult } from "../../api/result";
 import { Question } from "../../styles/SurveyStyles/QuizCardStyle";
 
@@ -10,36 +9,20 @@ import { getQuestion, getAnswer } from "../../api/question";
 import {
   ChoiceButton,
   PreviousButton,
+  SurveyButton
 } from "../../styles/SurveyStyles/SurveyButton";
-=======
 
-import { Question } from "../../styles/SurveyStyles/QuizCardStyle";
-
-import { getQuestion, getAnswer } from "../../api/question";
-import SurveyButton from "../../styles/SurveyStyles/SurveyButton";
->>>>>>> feature-api/S10P12A409-3
 
 function QuestionPage() {
   const params = useParams();
   const questionId = parseInt(params.questionNum);
-<<<<<<< HEAD
-  console.log("!!!!" + questionId);
-
-=======
->>>>>>> feature-api/S10P12A409-3
   const nav = useNavigate();
   const location = useLocation();
 
   const [question, setQuestion] = useState("question");
-<<<<<<< HEAD
-  const [answers, setAnswers] = useState([
-    // { answerId: 1, content: "hello" },
-    // { answerId: 2, content: "hello" },
-=======
   const [answer, setAnswer] = useState([
     { answerId: 1, content: "hello" },
     { answerId: 2, content: "hello" },
->>>>>>> feature-api/S10P12A409-3
   ]);
 
   let result = location.state?.result || [];
@@ -64,15 +47,7 @@ function QuestionPage() {
     // 선택지 내용 가져오기
     getAnswer(questionId)
       .then((data) => {
-<<<<<<< HEAD
         setAnswers(data.answers);
-        console.log("got answer");
-        console.log("data", data.answers);
-=======
-        setAnswer(data);
-        console.log("got answer");
-        console.log(answer);
->>>>>>> feature-api/S10P12A409-3
       })
       .catch((e) => {
         console.log(e);
@@ -80,7 +55,6 @@ function QuestionPage() {
   }, [questionId]);
 
   function handleClick(answerId) {
-<<<<<<< HEAD
     result[questionId - 1] = answerId;
     if (questionId === 7) {
       // 프론트엔드에서 백엔드로 result 전송
@@ -109,17 +83,6 @@ function QuestionPage() {
         .catch((error) => {
           console.error("Error getting calculated team ID:", error);
         });
-=======
-    console.log(answerId);
-
-    result[questionId] = answerId;
-    console.log("next");
-    console.log(result);
-
-    if (questionId === 7) {
-      console.log("끝남");
-      console.log(result);
->>>>>>> feature-api/S10P12A409-3
     } else {
       nav(`/question/${questionId + 1}`, { state: { result } });
     }
@@ -137,7 +100,6 @@ function QuestionPage() {
         <br />
         <div>{question}</div>
         <br />
-<<<<<<< HEAD
         {answers &&
           answers.map((item) => (
             <ChoiceButton
@@ -152,21 +114,6 @@ function QuestionPage() {
           <PreviousButton onClick={() => handleBeforeClick()}>
             이전 문제로 돌아가기
           </PreviousButton>
-=======
-        {answer &&
-          answer.map((item) => (
-            <SurveyButton
-              key={item.answerId}
-              onClick={() => handleClick(item.answerId)}
-            >
-              {item.content}
-            </SurveyButton>
-          ))}
-        {questionId >= 2 && (
-          <SurveyButton onClick={() => handleBeforeClick()}>
-            이전 문제로 돌아가기
-          </SurveyButton>
->>>>>>> feature-api/S10P12A409-3
         )}
       </>
     </Question>
