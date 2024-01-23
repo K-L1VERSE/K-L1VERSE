66e4bf7a9fe9ecbbaad057b915d6c8befe8dd19f
홍윤기 <lion0077v@gmail.com>
2024-01-23 09:50:51 +0900

    [Feat] 로그아웃 기능 개발
    - [BE] 로그아웃 기능 개발
    
    - 전역 상태 관리 해제
    
    [Resolves] S10P12A409-360
    [Ref]
    [Realted to]
    
    
    Former-commit-id: 94e456fb967a82a65a73e69e59010b7d650ebeb4

--- frontend/src/components/mypage/LogoutButton.jsx
+++ frontend/src/components/mypage/LogoutButton.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
+import { useRecoilState } from "recoil";
+import { userState } from "../../global/UserState";
+
 import axios from "../../api/axios";
 // import mockAxios from "../../api/mockAxios";
 
 function LogoutButton() {
+  const [isLogin, setIsLogin] = useRecoilState(userState);
   const logout = () => {
     axios.get("/users/logout").then(({ data }) => {
-      //  로그아웃 전역 상태 관리
+      // 로그아웃 전역 상태 관리
+      localStorage.removeItem("accessToken");
+      setIsLogin(false);
 
       console.log(data);
 
-      //  로그아웃 후 메인 페이지로 이동
+      // 로그아웃 후 메인 페이지로 이동
       // window.location.href = "/";
     });
   };
