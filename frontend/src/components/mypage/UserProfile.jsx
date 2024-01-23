661172c28fbee099a210e6ae8341e4623eb386f9
홍윤기 <lion0077v@gmail.com>
2024-01-23 14:21:17 +0900

    [Feat] GoalBox 컴포넌트 개발
    - [FE] 마이페이지 userProfile 컴포넌트 디자인 개발
    
    [Resolves] S10P12A409-263
    [Ref]
    [Realted to]
    
    
    Former-commit-id: 8023e40ec25a0d9a13ba5cfe35e2647d4b09c4ee

--- frontend/src/components/mypage/UserProfile.jsx
+++ frontend/src/components/mypage/UserProfile.jsx
@@ -130,35 +130,6 @@ function UserInfo({ user }) {
           </UserInfoContent>
         </UserInfoContainer>
       </ProfileTitleContainer>
-      <div>
-        프로필 사진 : <span>{user.profile}</span>
-      </div>
-      <div>
-        이름 : {user.nickname}
-        <button type="button" onClick={editUserInfo}>
-          수정
-        </button>
-      </div>
-      <div> 대표 뱃지 : {user.mainBadge} </div>
-      <div> 골 : {user.goal}</div>
-      <div> 적중률 : {user.accurate}</div>
-      <div> 총 배팅 : {user.totalBet}</div>
-      <div> 적중 배팅 : {user.winBet}</div>
-      <div>
-        뱃지
-        <div> 팀 1 : {user.badge.team1}</div>
-        <div> 팀 2 : {user.badge.team2}</div>
-        <div> 팀 3 : {user.badge.team3}</div>
-        <div> 팀 4 : {user.badge.team4}</div>
-        <div> 팀 5 : {user.badge.team5}</div>
-        <div> 팀 6 : {user.badge.team6}</div>
-        <div> 팀 7 : {user.badge.team7}</div>
-        <div> 팀 8 : {user.badge.team8}</div>
-        <div> 팀 9 : {user.badge.team9}</div>
-        <div> 팀 10 : {user.badge.team10}</div>
-        <div> 팀 11 : {user.badge.team11}</div>
-        <div> 팀 12 : {user.badge.team12}</div>
-      </div>
     </div>
   );
 }
