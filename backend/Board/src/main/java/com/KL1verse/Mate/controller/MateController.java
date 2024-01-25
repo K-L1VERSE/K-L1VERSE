d60ad4d65b84c66f0d76a39363478f86a2d292c1
고해림 <sunny9612@naver.com>
2024-01-25 09:35:40 +0900

    [Feat] [BE] 최근 2개 게시물 불러오기
    - [BE] 메인페이지 커뮤니티 API 개발
    
    [Resolves] S10P12A409-367
    [Ref]
    [Related to]
    
    
    Former-commit-id: 0ec6d7b8c0a4cbde6030664424ba40226c50ae90

--- backend/Board/src/main/java/com/KL1verse/Mate/controller/MateController.java
+++ backend/Board/src/main/java/com/KL1verse/Mate/controller/MateController.java
@@ -1,6 +1,15 @@
 package com.KL1verse.Mate.controller;
 
+import com.KL1verse.Mate.dto.req.MateDTO;
+import com.KL1verse.Mate.repository.entity.Mate;
+import com.KL1verse.Mate.service.MateService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mateboard")
 public class MateController {
 
+    private final MateService mateService;
+
+    public MateController(MateService mateService) {
+        this.mateService = mateService;
+    }
+
+    @GetMapping("/recent/{count}")
+    public ResponseEntity<List<MateDTO>> getMostRecentWaggles(@PathVariable int count) {
+        List<MateDTO> recentMates = mateService.getMostRecentMates(count);
+        return ResponseEntity.ok(recentMates);
+    }
 
 
 }
--- backend/Board/src/main/java/com/KL1verse/Product/controller/ProductController.java
+++ backend/Board/src/main/java/com/KL1verse/Product/controller/ProductController.java
@@ -1,6 +1,12 @@
 package com.KL1verse.Product.controller;
 
+import com.KL1verse.Product.dto.req.ProductDTO;
+import com.KL1verse.Product.service.ProductService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/productboard")
 public class ProductController {
+    private final ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/recent/{count}")
+    public ResponseEntity<List<ProductDTO>> getMostRecentProducts(@PathVariable int count) {
+        List<ProductDTO> recentProducts = productService.getMostRecentProducts(count);
+        return ResponseEntity.ok(recentProducts);
+    }
 }
