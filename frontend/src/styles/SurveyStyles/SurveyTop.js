c7987835283f41d43f9f7dece15fe6be6884f769
KohSulim <sulim0314@naver.com>
2024-01-31 09:19:06 +0900

    [Fix] Merge S10P12A409-421 -> master
    -
    
    [Resolves] S10P12A409-421
    [Ref]
    [Related to]
    
    
    Former-commit-id: cf1a042d9b79bf071f3a9d898f632f42b35faf48

---
+++ frontend/src/styles/SurveyStyles/SurveyTop.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const SurveyTop = styled.div`
+  left: 0;
+  right: 0;
+  font-size: 0.9rem;
+  font-weight: bold;
+  padding-top: 1rem;
+  padding-bottom: 2rem;
+  display: flex;
+  justify-content: left;
+`;
+
+export const ToLeftImg = styled.img`
+  width: 1rem;
+  height: 1rem;
+  margin-top: 0.1rem;
+  margin-left: 1rem;
+  margin-right: 0.7rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
