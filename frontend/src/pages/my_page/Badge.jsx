de66dfb369e9a7300b5bc694b10a231ee17d6a0a
김동훈 <huni19541@gmail.com>
2024-02-04 23:36:23 +0900

    [Fix] Front 사진 이름 변경
    -
    
    [Resolves]
    [Ref]
    [Realted to]
    
    
    Former-commit-id: 374d0bfd1f82b7c92e1409d34dc8a3639ab62217

--- frontend/src/pages/my_page/Badge.jsx
+++ frontend/src/pages/my_page/Badge.jsx
@@ -13,17 +13,17 @@ import BadgeList from "./BadgeList";
 function Badge() {
   const badgeCodeList = [
     "01",
-    "02",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
+    "03",
+    "04",
+    "05",
+    "09",
     "10",
-    "11",
-    "12",
+    "17",
+    "18",
+    "21",
+    "22",
+    "29",
+    "35",
   ];
 
   const badgeNameList = [
