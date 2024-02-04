5d9bc5ce98a8ef60c5919a368cd72c23074f5da8
lion0077v <lion0077v@gmail.com>
2024-02-04 15:16:48 +0900

    [Feat] 클린봇 api 완성
    Former-commit-id: fe222081f35716d92e48827554018fbdd146ef5a

--- backend/Cleanbot/Cleanbot/settings.py
+++ backend/Cleanbot/Cleanbot/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, ".env")
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'Cleanbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT':  env('DB_PORT')
     }
 }
 
