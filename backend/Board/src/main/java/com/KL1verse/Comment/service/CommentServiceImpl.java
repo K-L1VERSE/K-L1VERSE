bb534f014205bf7c3d2dd0f89e89fbc4e66feb5c
김동훈 <huni19541@gmail.com>
2024-02-05 14:23:59 +0900

    [Fix] [FE] 카카오톡 공유하기 추가
    -
    
    [Resolves] S10P12A409-422/alarm
    [Ref]
    [Realted to]
    
    
    Former-commit-id: 2739dcfb66d7ad4c8852a59fd38d2815988c7ff3

--- backend/Board/src/main/java/com/KL1verse/Comment/service/CommentServiceImpl.java
+++ backend/Board/src/main/java/com/KL1verse/Comment/service/CommentServiceImpl.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -27,6 +28,9 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
     private final KafkaBoardNotificationProducer kafkaBoardNotificationProducer;
+
+    @Value("${domain}")
+    private String domain;
 
     @Override
     public CommentDTO getCommentById(Long commentId, Long requestingUserId) {
@@ -68,7 +72,7 @@ public class CommentServiceImpl implements CommentService {
             BoardNotificationResDto.builder()
                 .type(BoardNotificationType.COMMENT)
                 .userId(board.getUserId())
-                .uri("http://localhost:3000/" + board.getBoardType().toString().toLowerCase() + String.valueOf(board.getBoardId()))
+                .uri(domain + "/" + board.getBoardType().toString().toLowerCase() + String.valueOf(board.getBoardId()))
                 .message(userNickname + "님이 새로운 댓글을 달았습니다.")
                 .build()
         );
