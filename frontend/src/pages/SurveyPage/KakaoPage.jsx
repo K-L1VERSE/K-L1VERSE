0131ec309ecf37d22c89028d37da558cfbde76dd
haelim12 <sunny9612@naver.com>
2024-02-05 15:15:31 +0900

    [Feat] [FE] 카카오톡 공유하기 구현
    - css 추가해야함.
    
    [Resolves] S10P12A409-562/kakao
    [Ref]
    [Related to]
    
    
    Former-commit-id: 570f90cc952db484fca889fe0f224ff089b7024d

--- frontend/src/pages/SurveyPage/KakaoPage.jsx
+++ frontend/src/pages/SurveyPage/KakaoPage.jsx
@@ -2,11 +2,32 @@ import React, { useEffect } from "react";
 
 const { Kakao } = window;
 
+const teamId = 1;
+const teamImages = {
+  1: "/images/surveyResult/ulsan_mita.jpg",
+  2: "/images/surveyResult/pohang_soidori.png",
+  3: "/images/surveyResult/jeju_gamguri.png",
+  4: "/images/surveyResult/jeonbuk_nighty.png",
+  5: "/images/surveyResult/seoul_cid.png",
+  6: "/images/surveyResult/daejeon_citizen.webp",
+  7: "/images/surveyResult/daegu_victorica.png",
+  8: "/images/surveyResult/incheon_ut2.png",
+  9: "/images/surveyResult/gangwon_gangwoong.png",
+  10: "/images/surveyResult/gwangju_mascot.png",
+  11: "/images/surveyResult/suwon_swoony.jpg",
+  12: "/images/surveyResult/kimcheon_shuwoong2.png",
+};
+
 const KakaoPage = () => {
   // 배포한 자신의 사이트
-  const realUrl = "http://localhost:3000/";
+  const realUrl = "http://i10a409.p.ssafy.io/survey";
   // 로컬 주소 (localhost 3000 같은거)
   const resultUrl = window.location.href;
+
+  const domainAndPort = process.env.REACT_APP_DOMAIN_AND_PORT;
+
+  const imageUrl = "https://i10a409.p.ssafy.io" + teamImages[teamId];
+  console.log("imageUrl: ", imageUrl);
 
   // 재랜더링시에 실행되게 해준다.
   useEffect(() => {
@@ -19,8 +40,27 @@ const KakaoPage = () => {
   }, []);
 
   const shareKakaoLink = () => {
-    Kakao.Share.sendCustom({
-      templateId: 104000,
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "당신에게 맞는 구단은?",
+        description: "울산 현대",
+        imageUrl: imageUrl,
+        // imageUrl: `${domainAndPort}${teamImages[teamId]}`,
+        link: {
+          mobileWebUrl: realUrl,
+          webUrl: realUrl,
+        },
+      },
+      buttons: [
+        {
+          title: "나도 궁금해요",
+          link: {
+            mobileWebUrl: realUrl,
+            webUrl: realUrl,
+          },
+        },
+      ],
     });
   };
 
