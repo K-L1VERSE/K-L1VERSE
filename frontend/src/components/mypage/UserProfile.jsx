97ef5447b3a73391a60f58aea8aa23ed00d901ec
김동훈 <huni19541@gmail.com>
2024-02-05 15:30:36 +0900

    [Fix] [FE] 유저 프로필 변경 코드 수정
    -
    
    [Resolves] S10P12A409-422/alarm
    [Ref]
    [Realted to]
    
    
    Former-commit-id: 932848af05eecc389c37497b0b9f7c91fc7dac35

--- frontend/src/components/mypage/UserProfile.jsx
+++ frontend/src/components/mypage/UserProfile.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { UserState } from "../../global/UserState";
 
 import styled from "styled-components";
 import BadgeButton from "./BadgeButton";
@@ -124,6 +126,8 @@ function UserInfo({ user }) {
     console.log("user정보 수정");
   }, [user]);
 
+  const setUserState = useSetRecoilState(UserState);
+
   const [selectedImage, setSelectedImage] = useState(null);
 
   /* 프로필 이미지 선택 시 처리 함수 */
@@ -139,19 +143,23 @@ function UserInfo({ user }) {
       const formData = new FormData();
       formData.append("file", file);
 
-      axios.post("/user/file/upload", formData)
-      .then((res) => {
-        axios.post("/user/users/profile", { profile: res.data.url })
+      axios
+        .post("/user/file/upload", formData)
         .then((res) => {
-          reader.readAsDataURL(file);
+          const url = res.data.url;
+          axios
+            .post("/user/users/profile", { profile: res.data.url })
+            .then((res) => {
+              reader.readAsDataURL(file);
+              setUserState((prev) => ({ ...prev, profile: url }));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         })
         .catch((err) => {
           console.log(err);
-        })
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        });
     }
   };
 
@@ -159,7 +167,7 @@ function UserInfo({ user }) {
   useEffect(() => {
     console.log("프로필 이미지 수정");
   }, [selectedImage]);
-  
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleModalOpen = () => {
@@ -185,14 +193,14 @@ function UserInfo({ user }) {
         </ProfileTitleHeader>
         <UserInfoContainer>
           <UserInfoContent>
-          <label htmlFor="profileImageInput">
-            <UserProfile src={selectedImage || user.profile}/>
-          </label>
-          <UserProfileInput
-            type="file"
-            id="profileImageInput"
-            accept="image/*"
-            onChange={handleImageChange}
+            <label htmlFor="profileImageInput">
+              <UserProfile src={selectedImage || user.profile} />
+            </label>
+            <UserProfileInput
+              type="file"
+              id="profileImageInput"
+              accept="image/*"
+              onChange={handleImageChange}
             />
             <BadgeButton />
             <UserNickName>김민재굉장하다{user.nickname}</UserNickName>
@@ -203,11 +211,7 @@ function UserInfo({ user }) {
       </ProfileTitleContainer>
 
       {/* EditNickname Modal */}
-      {isModalOpen && (
-        <EditNicknameModal
-          setModalOpen={setIsModalOpen}
-        />
-      )}
+      {isModalOpen && <EditNicknameModal setModalOpen={setIsModalOpen} />}
     </div>
   );
 }
