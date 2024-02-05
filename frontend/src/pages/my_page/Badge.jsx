86255c9292dc6351e5023392943b21a0124233ba
김동훈 <huni19541@gmail.com>
2024-02-05 10:25:03 +0900

    [Fix] [BE] 크롤링 코드, Kafka 코드 수정
    -
    
    [Resolves] S10P12A409-422/alarm
    [Ref]
    [Realted to]
    
    
    Former-commit-id: e0135f6e4606dcaf837c647bf81d9aadd201bec1

--- frontend/src/pages/my_page/Badge.jsx
+++ frontend/src/pages/my_page/Badge.jsx
@@ -12,18 +12,18 @@ import BadgeList from "./BadgeList";
 
 function Badge() {
   const badgeCodeList = [
-    "01",
-    "03",
-    "04",
-    "05",
-    "09",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
     "10",
-    "17",
-    "18",
-    "21",
-    "22",
-    "29",
-    "35",
+    "11",
+    "12",
   ];
 
   const badgeNameList = [
