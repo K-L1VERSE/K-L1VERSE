b00f3522c75340219c0ad2c621028cfa69bc5ee1
KohSulim <sulim0314@naver.com>
2024-02-06 15:37:00 +0900

    [Fix] 노스트라다무스, 봇 수정
    Former-commit-id: 6b8c58ec9d25411cc744a0ab09ebb22e64e9dbec

--- frontend/src/pages/nav/Navbar.jsx
+++ frontend/src/pages/nav/Navbar.jsx
@@ -15,45 +15,6 @@ import Logo from "../../assets/K-L1VERSE(white).png";
 import { NotificationState } from "../../global/NotificationState";
 
 export default function Header() {
-  (function () {
-    var w = window;
-    if (w.ChannelIO) {
-      return w.console.error("ChannelIO script included twice.");
-    }
-    var ch = function () {
-      ch.c(arguments);
-    };
-    ch.q = [];
-    ch.c = function (args) {
-      ch.q.push(args);
-    };
-    w.ChannelIO = ch;
-    function l() {
-      if (w.ChannelIOInitialized) {
-        return;
-      }
-      w.ChannelIOInitialized = true;
-      var s = document.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js";
-      var x = document.getElementsByTagName("script")[0];
-      if (x.parentNode) {
-        x.parentNode.insertBefore(s, x);
-      }
-    }
-    if (document.readyState === "complete") {
-      l();
-    } else {
-      w.addEventListener("DOMContentLoaded", l);
-      w.addEventListener("load", l);
-    }
-  })();
-
-  ChannelIO("boot", {
-    pluginKey: "e6c79081-3a8d-4b7f-8f9d-c90be614e40a",
-  });
-
   const [notificationState, setNotificationState] =
     useRecoilState(NotificationState);
 
