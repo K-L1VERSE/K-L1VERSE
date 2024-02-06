e538f00dbdd9952308a2d8e7c649073298b796e9
lion0077v <lion0077v@gmail.com>
2024-02-06 22:53:49 +0900

    [Feat] messageId 유일성 보장 구현
    Former-commit-id: aa4ffe3a0ac081c0e0f7a206dae8cffb013a8df6

--- backend/Match/src/main/java/com/KL1verse/match/chat/controller/MessageController.java
+++ backend/Match/src/main/java/com/KL1verse/match/chat/controller/MessageController.java
@@ -1,11 +1,14 @@
 package com.KL1verse.match.chat.controller;
 
 import com.KL1verse.match.chat.dto.req.MessageReqDto;
+import com.KL1verse.match.chat.dto.res.MessageResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @RestController
@@ -13,9 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     private final SimpMessageSendingOperations sendingOperations;
+    private static final AtomicLong messageIdCounter = new AtomicLong(0);
+
+    private static Long generateMessageId() {
+        return messageIdCounter.incrementAndGet();
+    }
 
     @MessageMapping("/chat/message")
-    public void enter(MessageReqDto message) {
-        sendingOperations.convertAndSend("/topic/chat/room/" + message.getRoomId(), message);
+    public void enter(MessageReqDto messageReqDto) {
+        MessageResDto message = MessageResDto.builder()
+                .messageId(generateMessageId())
+                .roomId(messageReqDto.getRoomId())
+                .profile(messageReqDto.getProfile())
+                .sender(messageReqDto.getSender())
+                .message(messageReqDto.getMessage())
+                .date(messageReqDto.getDate())
+                .profile(messageReqDto.getProfile())
+                .build();
+
+        sendingOperations.convertAndSend("/topic/chat/room/" + messageReqDto.getRoomId(), message);
+
+        // Kafka Producing
     }
 }
