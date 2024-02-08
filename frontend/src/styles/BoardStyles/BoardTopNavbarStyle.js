6b83ecb804e51d92f4b9bc7b1afb7525f2fdd4d3
KohSulim <sulim0314@naver.com>
2024-02-08 10:21:27 +0900

    [Fix] BoardTopNavbar 컴포넌트 수정
    [Resolves] S10P12A409-578
    [Ref]
    [Related to]
    
    
    Former-commit-id: 5c0a7c7b18bbc2a15122648b7621f29afbc9370d

--- frontend/src/styles/BoardStyles/BoardTopNavbarStyle.js
+++ frontend/src/styles/BoardStyles/BoardTopNavbarStyle.js
@@ -3,15 +3,13 @@ import styled from "styled-components";
 export const BoardTopNavbarContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
   align-items: flex-start;
   padding: 12px 16px;
   gap: 20px;
   width: 390px;
-  height: 89px;
-  left: 0px;
   top: 68px;
   font-family: "Pretendard-Regular";
+  margin-bottom: 1rem;
 `;
 
 export const Title = styled.h1`
@@ -24,31 +22,32 @@ export const Title = styled.h1`
 
 export const Nav = styled.nav`
   display: flex;
-  flex-direction: row;
   align-items: center;
   padding: 0px;
   gap: 13px;
-  width: 358px;
-  height: 19px;
 `;
 
 export const WaggleButton = styled.button`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 5px 6px;
-  gap: 4px;
+  padding: 8px 10px;
+  font-size: 13px;
+  height: 28px;
   width: auto;
-  height: 26px;
-  background-color: #dddddd;
-  color: #a5a5a5;
-  border-radius: 3px;
+  gap: 4px;
+  background-color: #f4f4f4;
+  color: #a9a9a9;
+  border-radius: 4px;
   border: none;
 
   &.active {
-    background: #f7e4dc;
-    color: #f09168;
+    background: #fee8de;
+    color: #f07e3d;
     font-weight: bold;
+  }
+  &:hover {
+    cursor: pointer;
   }
 `;
 
@@ -56,13 +55,14 @@ export const MateButton = styled.button`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 5px 6px;
+  padding: 8px 10px;
+  font-size: 13px;
+  height: 28px;
   gap: 4px;
   width: auto;
-  height: 26px;
-  background-color: #dddddd;
-  color: #a5a5a5;
-  border-radius: 3px;
+  background-color: #f4f4f4;
+  color: #a9a9a9;
+  border-radius: 4px;
   border: none;
 
   &.active {
@@ -70,23 +70,31 @@ export const MateButton = styled.button`
     color: #16b368;
     font-weight: bold;
   }
+  &:hover {
+    cursor: pointer;
+  }
 `;
 export const ProductButton = styled.button`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 5px 6px;
+  height: 26px;
+  padding: 8px 10px;
+  font-size: 13px;
+  height: 28px;
   gap: 4px;
   width: auto;
-  height: 26px;
-  background-color: #dddddd;
-  color: #a5a5a5;
-  border-radius: 3px;
+  background-color: #f4f4f4;
+  color: #a9a9a9;
+  border-radius: 4px;
   border: none;
 
   &.active {
     background: #e5edfb;
     color: #578cea;
     font-weight: bold;
+  }
+  &:hover {
+    cursor: pointer;
   }
 `;
