b08a09ec1a318b0f1d5a7e3c2b7541fb50b12f4d
김동훈 <huni19541@gmail.com>
2024-02-11 01:16:55 +0900

    [Feat] Board 좋아요 알림 코드 추가
    Former-commit-id: d6d78602b258c12ae6becb2c20bcbd56d4c4015d

--- backend/Board/src/main/java/com/KL1verse/Waggle/repository/WaggleLikeRepository.java
+++ backend/Board/src/main/java/com/KL1verse/Waggle/repository/WaggleLikeRepository.java
@@ -1,8 +1,11 @@
 package com.KL1verse.Waggle.repository;
 
 import com.KL1verse.Waggle.repository.entity.WaggleLike;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +13,6 @@ public interface WaggleLikeRepository extends JpaRepository<WaggleLike, Long> {
 
     Optional<WaggleLike> findByUserIdAndWaggleId_WaggleId(Long userId, Long waggleId);
 
+    @Query(value = "SELECT nickname, profile FROM user WHERE user_id = :userId", nativeQuery = true)
+    List<Object[]> findNicknameAndProfileByUserId(Integer userId);
 }
