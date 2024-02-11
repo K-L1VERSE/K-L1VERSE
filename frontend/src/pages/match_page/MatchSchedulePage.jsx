bdfff575fe6ec255d970c63c207c555940fc610f
haelim12 <sunny9612@naver.com>
2024-02-11 21:43:57 +0900

    [Feat] [FE] 직관메이트 css 수정
    - 경기일정, 제목 겹침 수정
    
    [Resolves] S10P12A409-52
    [Ref]
    [Related to]
    
    
    Former-commit-id: 9634971dd11c43ddbf3dfec4199bf189a64ca455

--- frontend/src/pages/match_page/MatchSchedulePage.jsx
+++ frontend/src/pages/match_page/MatchSchedulePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { getMatchList } from "../../api/match";
 import SelectContainer from "../../components/match/ScheduleSelect";
 import TableContainer from "../../components/match/ScheduleTable";
