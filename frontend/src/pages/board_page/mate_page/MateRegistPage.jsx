cf7fbec374c5c2d9e2f1c4f3d223b99f696e6253
sohy19 <sohy19@hufs.ac.kr>
2024-02-15 17:26:40 +0900

    [Feat] 제목이나 글 내용 없이 글 작성 시 모달
    Former-commit-id: 4f4616e5926bb4c51f844e6f53708b9f3c76b758

--- frontend/src/pages/board_page/mate_page/MateRegistPage.jsx
+++ frontend/src/pages/board_page/mate_page/MateRegistPage.jsx
@@ -36,11 +36,27 @@ function MateRegistPage() {
 
   const handleSubmit = () => {
     if (title.length === 0) {
-      alert("제목 없어요");
+      Swal.fire({
+        html: `
+          <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hatching%20Chick.png" alt="Hatching Chick" width="100" height="100" />
+          <div style="font-size:1rem; font-family:Pretendard-Regular; margin-top: 1rem;">제목을 입력해주세요!</div>
+        `,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText:
+          "<div style='font-size:1rem; font-family:Pretendard-Regular;'>확인</div>",
+      });
       return;
     }
     if (content.length === 0) {
-      alert("내용 없어요");
+      Swal.fire({
+        html: `
+          <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hatching%20Chick.png" alt="Hatching Chick" width="100" height="100" />
+          <div style="font-size:1rem; font-family:Pretendard-Regular; margin-top: 1rem;">내용을 입력해주세요!</div>
+        `,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText:
+          "<div style='font-size:1rem; font-family:Pretendard-Regular;'>확인</div>",
+      });
       return;
     }
     if (isUpdateMode) {
